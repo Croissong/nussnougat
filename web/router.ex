a760da4149071ea915325df55243895f29d719ef
Jan Möller <jan.moeller0@gmail.com>
2016-01-10 03:21:36 +0100

    add croissong + donkebap phoenix routes

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule Nussnougat.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+		get "/croissong", PageController, :index
+		get "/donkebap", PageController, :index
   end
 
   # Other scopes may use custom stacks.
